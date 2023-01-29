abb62d9fac2d8bb85dd59a1762a2188135c70913
stefanoschoonheimMBO <320791@student.mboutrecht.nl>
2023-01-29 22:58:18 +0100

    Added Video and Database sql file

--- db/Persoon.sql
+++ db/Persoon.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 24 nov 2022 om 16:21
--- Serverversie: 5.7.31
--- PHP-versie: 8.1.10
+-- Gegenereerd op: 29 jan 2023 om 21:57
+-- Serverversie: 5.7.36
+-- PHP-versie: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `Php-pdo-crud-2209b`
 --
-CREATE DATABASE IF NOT EXISTS `Php-pdo-crud-2209b` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `Php-pdo-crud-2209b`;
 
 -- --------------------------------------------------------
 
@@ -35,28 +33,21 @@ CREATE TABLE IF NOT EXISTS `Persoon` (
   `Voornaam` varchar(255) NOT NULL,
   `Tussenvoegsel` varchar(20) DEFAULT NULL,
   `Achternaam` varchar(255) NOT NULL,
+  `Mobiel` varchar(12) NOT NULL,
+  `StraatNaam` varchar(50) NOT NULL,
+  `Huisnummer` varchar(6) NOT NULL,
+  `Woonplaats` varchar(50) NOT NULL,
+  `Postcode` varchar(6) NOT NULL,
+  `Landnaam` varchar(32) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `Persoon`
 --
 
-INSERT INTO `Persoon` (`Id`, `Voornaam`, `Tussenvoegsel`, `Achternaam`) VALUES
-(1, 'Arjan', 'de', 'Ruijter'),
-(2, 'Pieter', NULL, 'Boomstra'),
-(3, 'Harry', 'de', 'Boer'),
-(4, 'Bert', 'van den', 'Akker'),
-(5, 'Jason', '', 'Strikland'),
-(13, 'Allard', '', 'Wijmenga'),
-(14, 'Bert', 'van der ', 'Linden'),
-(15, 'Bert', 'van der ', 'Linden'),
-(16, 'q', 'q', 'q'),
-(17, 'Arjan', 'de', 'Ruijter'),
-(18, 'Test', 'ter', 'DeTest'),
-(19, 'Barry', 'van', 'Deelen'),
-(20, 'Gerrit', 'de', 'Boer'),
-(21, 'Test', 'de', 'Tester123456');
+INSERT INTO `Persoon` (`Id`, `Voornaam`, `Tussenvoegsel`, `Achternaam`, `Mobiel`, `StraatNaam`, `Huisnummer`, `Woonplaats`, `Postcode`, `Landnaam`) VALUES
+(2, 'Dio', 'test', 'Brando', '0646909138', 'biesterlaan', '22', 'schalkwijk', '3998kk', 'Nederland');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
